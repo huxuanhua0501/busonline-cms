044ca66e0f31b52abdd3055fd2f6068f8e4675b1
xuanhua.hu <xuanhu.hu@busonline.com>
2016-08-17 14:28:29 +0800

    123213

--- src/main/java/net/busonline/api/service/IBusApiService.java
+++ src/main/java/net/busonline/api/service/IBusApiService.java
@@ -2,6 +2,9 @@ package net.busonline.api.service;
 
 
 public interface IBusApiService {
+	
+	
+	
 	public String city(String city,String sign);
 	public String linecity(String city,String data,String sign) ;
 	public String linestop(String city,String data,String sign);
