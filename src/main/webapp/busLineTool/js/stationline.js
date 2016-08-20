e88bcf05bc8748716a93153c4140f5736ed2dbcc
miao.ouyang <miao.ouyang@busonline.com>
2016-08-20 15:16:44 +0800

    no message

--- src/main/webapp/busLineTool/js/stationline.js
+++ src/main/webapp/busLineTool/js/stationline.js
@@ -1,101 +1,9 @@
 (function() {
 	  var $buslineName=$("#lineName"),
 	      oBusName,buslevel="",network="",buslinedir="";
-	
-	   //搜索线路
-	   $("#searchBusLine,#searchCondition").click(function(){
-            oBusName=$buslineName.val();//线路名称
-            $(".buslineLeve").each(function(){
-              if($(this).attr('checked')){
-                  buslevel=$(this).attr('value');
-                  console.log(buslevel);
-              }
-            });
-            
-            //inintData()
-	   });
-	      //查询数据分页
-    //查询数据分页
-   var classId = " ";
-   var classLevel;
-   var pageNum = 0;
-   var $content = $('#ajax_content'); //数据列表容器
-   var size = 5; //每页显示条数据
-   //组装数据
-   var inintData = function(e, data, cur) {
-     e.nextAll().remove(); //先清空遗留数据
-     var html = '',
-       categorys = '';
-     var curList = (cur - 1) * size;
-     var info = data.result.pageList;
-     for (var i = 0; i < info.length; i++) {
-       for (j = 0; j < info[i].category.length; j++) {
-         categorys += info[i].category[j].name + ' ';
-       }
-       curList++;
-       html += "<tr align='center'><td class='attributeId' category-id=" + info[i].id + ">" + curList + "</td><td class='attrNameList'>" + info[i].name +
-         "</td><td class='values'>" + info[i].values.toString() + "</td><td class='categorys'>" + categorys + "</td><td><a class='updateBtn' >修改</a><a class='deletedBtn'>删除</a></td></tr>"
-       categorys = '';
-     }
-     e.after(html);
-
-   };
-   var loadData = function() {
-     var loadFlag = false;
-     var attrName = $(".attName").val();
-     $.getJSON('/queryAttributeByPageForAjax.do', {
-       "current_page": 1,
-       "page_size": size,
-       'name': attrName,
-       'category_id': classId
-     }, function(data) { //第一次先访问第一页获取总条数
-       //组装数据
-       inintData($content, data, 1); //第一次得到的数据
-       //设置分页
-       $content.ampager({
-         'pagerName': 'ajax_pager',
-         'mode': 'ajax',
-         'needNumInput': true,
-         'dataCount': data.result.totalCount, //后台获取总条数
-         'viewCount': size, //配置每页显示
-         'listCount': 7,
-         'callback': function(e, i, c) { //分别代表e：数据父容器，i：当前页，c：每页显示多少条数据
-           //(首次加载不进入此步骤)
-           if (!loadFlag)
-             return false;
-           $.getJSON('/queryAttributeByPageForAjax.do', {
-             "current_page": i,
-             "page_size": c,
-             'name': attrName,
-             'category_id': classId
-           }, function(data2) {
-             inintData(e, data2, i); //这里用$content也行
-           });
-         }
-       });
-
-       loadFlag = true;
-     });
-   };
-   //页面第一次加载并分页
-  // loadData();
-   //查询加载并分页
-
-   // $('#seclectAttr').click(function() {
-   //   if ($(".threeLevel").val() == 0) {
-   //     classLevel = $(".twoLevel").val();
-   //     if (classLevel == 0) {
-   //       classId = $(".oneLevel").val();
-   //     } else {
-   //       classId = $(".twoLevel").val();
-   //     }
-
-   //   } else {
-   //     classId = $(".threeLevel").val();
-   //   }
-   //   loadData();
-   // });
-       //请求字典信息接口
+	  var  cityval = $citylist.find("option:selected").text();
+	  
+	    //请求字典信息接口
 	  var ajaxDictionaries=function(){
 	  	 $.ajax({
 	  	 	url:'/iBusGather/dictionary.do',
@@ -112,18 +20,134 @@
 						citysCon += '<option value="' + citys[i].id + '">' + citys[i].name + '</option>';
 					}
 					for (var j = 0; j < linetypes.length; j++) {
-						linetypecon += '<label><input name="networkType" type="checkbox" value="' + linetypes[j].id + '"/>' + linetypes[j].name + ' </label>';
+						linetypecon += '<label><input name="networkType" class="networkType" type="checkbox" value="' + linetypes[j].id + '"/>' + linetypes[j].name + ' </label>';
 					}
 					$("#citylist").html(citysCon);
-					$("#network").html(linetypecon);
+					$(".network").html(linetypecon);
 					$("input[name=networkType]:eq(0),input[name=linkdir]:eq(0)").attr("checked", 'checked');
 				}
 				
 	  	 	}
 	  	 });        
 	  };
-
 	  ajaxDictionaries();//初始化城市，类型，上行下行
+
+    //查询数据分页
+   var pageNum = 0;
+   var $content = $('#ajax_content'); //数据列表容器
+   var size = 20; //每页显示条数据
+   //组装数据
+   var inintData = function(e, data, cur) {
+     e.nextAll().remove(); //先清空遗留数据
+     var html = '';
+     var curList = (cur - 1) * size;
+     var info = data.data.listAudit;
+     var dir;
+     for (var i = 0; i < info.length; i++) {
+       curList++;
+       if(info[i].linkdir==1){
+         dir="下行";
+       }
+       else{
+       	  dir="上行";
+       }
+       html += '<tr><td>'+curList+'</td><td>'+info[i].linenamebd+'</td><td>'+info[i].linetype+'</td><td>'+info[i].linetype+'</td><td>'+info[i].dictionaryname+'</td><td>'+dir+'</td><td>'+info[i].matchnumber+'</td><td>'+info[i].installationnumber+
+       '</td><td>'+info[i].startstop+'</td><td>'+info[i].endstop+'</td><td>'+info[i].totalstop+'</td><td><a class="stationList" data-id="'+info[i].id'">查看站点</a><a  class="busUpdate"  data-id="'+info[i].id'">修改</a>'+
+       '<a  class="busDelete"  data-id="'+info[i].id'">删除</a></td></tr>';
+
+     }
+     e.after(html);
+
+   };
+   var loadData = function() {
+     var loadFlag = false;
+     $.getJSON('/lineview/getallLine', {
+       "currentPage ": 1,
+       "pageSize": size,
+       'cityname': cityval,
+       'linename': oBusName,
+       "linkdir ": buslinedir,
+       "linetype": buslevel,
+       'dictionaryid': network
+     }, function(data) { //第一次先访问第一页获取总条数
+       //组装数据
+       inintData($content, data, 1); //第一次得到的数据
+       //设置分页
+       $content.ampager({
+         'pagerName': 'ajax_pager',
+         'mode': 'ajax',
+         'needNumInput': true,
+         'dataCount': data.result.totalCount, //后台获取总条数
+         'viewCount': size, //配置每页显示
+         'listCount': 7,
+         'callback': function(e, i, c) { //分别代表e：数据父容器，i：当前页，c：每页显示多少条数据
+           //(首次加载不进入此步骤)
+           if (!loadFlag)
+             return false;
+           $.getJSON('/queryAttributeByPageForAjax.do', {
+             "currentPage": i,
+             "pageSize": c,
+             'cityname': cityval,
+             'linename': oBusName,
+             'linkdir': buslinedir,
+             'linetype': buslevel,
+             'dictionaryid': network
+           }, function(data2) {
+             inintData(e, data2, i); //这里用$content也行
+           });
+         }
+       });
+       loadFlag = true;
+     });
+   };
+   //页面第一次加载并分页
+   loadData();
+ //搜索线路
+	   $("#searchBusLine,#searchCondition").click(function(){
+            oBusName=$buslineName.val();//线路名称
+            //线路等级
+            $(".buslineLeve").each(function(){
+              if($(this).attr('checked')){
+                  buslevel+=$(this).attr('value')+",";
+              }
+            });
+             //网络类型
+             $(".networkType").each(function(){
+              if($(this).attr('checked')){
+                  network+=$(this).attr('value')+",";
+              }
+            });
+              //网络类型
+             $(".buslineDir").each(function(){
+              if($(this).attr('checked')){
+                  buslinedir+=$(this).attr('value')+",";
+              }
+            });
+             if(!oBusName&&!buslevel&&!network&&!buslinedir){
+             	alert("请选择查询条件");
+             }
+            else{
+              loadData();
+            }
+            
+	   });
+	 var lineId;
+     var  busLineUpdate=function(){
+           $.ajax({
+	  	 	url:'/lineview/getLineById',
+	  	 	type:'post',
+	  	 	dataType:'json',
+	  	 	data:{
+	  	 		id:lineId
+	  	 	}
+	  	 	success:function(res){
+				if (res.code == 200) {
+					
+				}
+				
+	  	 	}
+	  	 });        
+     }
       $("#condition").click(function(){
       	$(".conditionBox").show();
       });
@@ -131,6 +155,8 @@
       	$(".conditionBox").hide();
       });
        $(".main").on("click",".busUpdate",function(){
+       	   lineId=$(this).attr("data-id");
+       	   busLineUpdate();
            $(".updateWindow").show();
        });
         $("#cancelUpdate,.close").click(function(){
