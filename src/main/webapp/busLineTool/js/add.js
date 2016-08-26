8d989a747a63bcc060f08c46a87c2c581d787a77
miao.ouyang <miao.ouyang@busonline.com>
2016-08-26 14:41:49 +0800

    no message

---
+++ src/main/webapp/busLineTool/js/add.js
@@ -0,0 +1,124 @@
+(function(){
+	var  $dictionaryname=$("#nameLevel1"),
+	     $dictionaryname2=$("#nameLevel2"),
+	     dictionaryname,parentid=0,
+	     leve2=$('.levelone option:selected').val();
+	    var selectList=function(){
+	    	  $(".tianjia_box").html(" ");
+	    	  $(".levelone").html(" ");
+            $.ajax({
+				url: '../dict/selectDicOne.do',
+				type: 'post',
+				dataType: 'json',
+            data:{
+                userid:user_Id
+            },
+				success: function(res) {
+                  if(res.code==200){
+                  	 var resData=res.data;
+                  	 var concent="",options="";
+                  	 for(var i=0;i<resData.length;i++){
+                  	 	concent+="<li>"+resData[i].dictionaryname+"</li>";
+                  	 	options+="<option value ='"+resData[i].id+"'>"+resData[i].dictionaryname+"</option>";
+                  	 }
+                     $(".tianjia_box").html(concent);
+                     $(".levelone").html(options);
+                     concent="";
+                     options="";
+                      selectList2();
+                     
+                  }
+                  else{
+                  	alert("添加失败");
+                  }
+				}
+			});
+	    };
+        selectList();
+	     var selectList2=function(){
+	     	$(".leve2con").html(" ");
+	     	 leve2=$('.levelone option:selected').val();
+            $.ajax({
+				url: '../dict/selectDicTwo.do',
+				type: 'post',
+				dataType: 'json',
+				data: {
+					id:leve2,
+               userid:user_Id
+				},
+				success: function(res) {
+                  if(res.code==200){
+                  	 var resData=res.data;
+                  	 var leveCon="";
+                  	 for(var i=0;i<resData.length;i++){
+                  	 	leveCon+="<span>"+resData[i].dictionaryname+"</span>";
+                  	 }
+                      $(".leve2con").append(leveCon);
+                     leveCon="";
+                  }
+				}
+			});
+	    };
+        if(leve2){
+          selectList2();
+        }
+		var addlevel = function() {
+			$.ajax({
+				url: '../dict/insertdict.do',
+				type: 'post',
+				dataType: 'json',
+				data: {
+               userid:user_Id,
+					parentid:parentid,
+					dictionaryname: dictionaryname
+				},
+				success: function(res) {
+                  if(res.code==200){
+                  	 alert("添加成功");
+                  	 if(parentid>0){
+                  	 	 selectList();
+                  	 	 selectList2();
+                  	 }
+                  	 else{
+                  	 	 selectList();
+                  	 }
+                  	 $dictionaryname.val("");
+                  	 $dictionaryname2.val("");
+
+                  }
+                  else{
+                  	alert("添加失败");
+                  }
+				}
+			});
+		};
+    //正则判断输入格式
+   var regFunction = function(textString) {
+      var reg = /^[\u4e00-\u9fa5_a-zA-Z0-9]+[\,{0,1}\u4e00-\u9fa5_a-zA-Z0-9]*$/;
+      if (textString&&reg.test(textString)) {
+         addlevel();
+      } else {
+         alert("不能为空或多个关键词只能以英文逗号隔开，且不能有空格");
+      }
+   };
+      //添加一级目录索引
+      $(".add").click(function(){
+      	 var idx=$(".add").index(this);
+            if (idx == 0) {
+               dictionaryname = $dictionaryname.val();
+               parentid = 0;
+               regFunction(dictionaryname);
+            }
+         if (idx == 1) {
+            dictionaryname = $dictionaryname2.val();
+            parentid = $('.levelone option:selected').val();
+            regFunction(dictionaryname);
+      	 
+      	}
+      });
+
+      $(".levelone").change(function(){
+      	 parentid=$('.levelone option:selected').val();
+      	 selectList2();
+      })
+})()
---
+++ src/main/webapp/busLineTool/js/list.js
@@ -0,0 +1,130 @@
+(function(){
+	var  parentid=0,
+	     indexid,dictionary;
+	    var listShow=function(){
+	    	$(".first").nextAll().remove();
+            $.ajax({
+				url: '../dict/selectDicItme.do',
+				type: 'post',
+				dataType: 'json',
+				data:{
+					 userid:user_Id
+					},
+				success: function(res) {
+                  if(res.code==200){
+                  	 var resData=res.data.arr;
+                  	 var html="";
+                  	 var html2="";
+					for (var i = 0; i < resData.length; i++) {
+						for (var j = 0; j < resData[i].two.length; j++) {
+							html2 += '<li><span>' + resData[i].two[j].dictionaryname + '</span><a href="javascript:;" class="xiugai" data-id="' + resData[i].two[j].id + '" data-level="2">修改</a><a href="javascript:;" '+
+							'class="delete" data-level="2" data-id="' + resData[i].two[j].id + '">删除</a></li>';
+						}
+						html +='<tr><td>' + (i + 1) + '</td><td>' + resData[i].dictionaryname + '<a href="javascript:;" class="xiugai" data-level="1" data-id="' + resData[i].id + '">修改</a></td><td><ul class="liebiao_box">' + html2 + '</ul></td></tr>';
+							// 	html +='<tr><td>' + (i + 1) + '</td><td>' + resData[i].dictionaryname + '<a href="javascript:;" class="xiugai" data-level="1" data-id="' + resData[i].id + '">修改</a><a href="javascript:;" class="delete" data-level="1" data-id="' + resData[i].id +
+							// '">删除</a></td><td><ul class="liebiao_box">' + html2 + '</ul></td></tr>';
+						html2 = "";
+					}
+                  	 $(".tableList").append(html);
+                  	 html="";
+
+                  }
+				}
+			});
+	    };
+	    listShow();
+		var update = function() {
+			$.ajax({
+				url: '../dict/updateDicname.do',
+				type: 'post',
+				dataType: 'json',
+				data: {
+					userid:user_Id,
+					id:indexnum,
+					dictionaryname: dictionary
+				},
+				success: function(res) {
+                  if(res.code==200){
+                  	 alert("修改成功");
+                  	 listShow();
+                  	 $(".updataWindow").hide();
+                  	
+                  	
+                  }
+                  else{
+                  	 alert("修改失败");
+                  }
+				}
+			});
+		};
+       var deleteLevel2 = function() {
+			$.ajax({
+				url: '../dict/delDicTwo.do',
+				type: 'post',
+				dataType: 'json',
+				data: {
+					userid:user_Id,
+					id:indexnum
+				},
+				success: function(res) {
+                  if(res.code==200){
+                  	 
+                  	  alert("删除成功");
+                  	   listShow();
+
+                  }
+                  else{
+                  	 alert("删除失败");
+                  }
+				}
+			});
+		};
+		  var deleteLevel1 = function() {
+			$.ajax({
+				url: '../dict/delDic.do',
+				type: 'post',
+				dataType: 'json',
+				data: {
+					id:indexnum,
+					userid:user_Id
+				},
+				success: function(res) {
+                  if(res.code==200){
+                  	 alert("删除成功");
+                  	  listShow();
+                  }
+                  else{
+                  	 alert("删除失败");
+                  }
+				}
+			});
+		};
+      //修改
+      $(".tableList").on("click",'.xiugai',function(){
+      	 indexnum=$(this).attr("data-id");
+      	 $(".dictionary").val("");
+      	 $(".updataWindow").show();
+      });
+       //修改保存
+       $(".savaBtn").on("click",function(){
+        	dictionary=$('.dictionary').val();
+      	    update();
+      });
+      //删除索引
+       $(".tableList").on("click",'.delete',function(){
+        	indexnum=$(this).attr("data-id");
+       	   var level=$(this).attr("data-level");
+
+			if (confirm("确定要删除吗？")) {
+				if (level == 1) {
+                    deleteLevel1();
+				} else {
+                    deleteLevel2();
+				}
+				
+			}
+      });
+      $(".cancel").click(function(){
+      	  $(".updataWindow").hide();
+      });
+})()
